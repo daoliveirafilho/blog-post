2aee83cdc6241ec827436536fa0f076e9d8ffb67
Ader Oliver <63608525+daoliveirafilho@users.noreply.github.com>
2026-06-28 12:27:32 -0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,57 @@
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
+import rehypeMermaid from 'rehype-mermaid';
+
+import vercel from '@astrojs/vercel';
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isVercel = process.env.VERCEL === '1';
+
+const mermaidStrategy = isProduction || isVercel ? 'pre-built' : 'inline-svg';
+
+console.log(`Using Mermaid strategy: ${mermaidStrategy}`);
+
+export default defineConfig({
+  site: 'https://theme-blog-1.da-oliveira-filho-03d.workers.dev',
+  integrations: [
+    react(),
+    mdx({
+      remarkPlugins: [remarkReadingTime],
+      rehypePlugins: [
+        [
+          rehypeMermaid,
+          {
+            strategy:
+              process.env.NODE_ENV === 'production'
+                ? 'pre-mermaid'
+                : 'inline-svg',
+          },
+        ],
+      ],
+      syntaxHighlight: {
+        type: 'shiki',
+        excludeLangs: ['mermaid'],
+      },
+    }),
+  ],
+
+  i18n: {
+    locales: ['fr', 'en'],
+    defaultLocale: 'fr',
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: vercel(),
+});
